214b7f6e5570db91cd037ef062f1a32bdcffb792
dmonroe08 <dmonroe08@fullsail.edu>
2014-02-13 20:00:12 -0500

    First commit for Expressions_Personal2

---
+++ Git Repo/script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by DanielleMonroe on 2/13/14.
+ */
+
+//My mother's children and grandchildren//
+
+var children = [Anthony, Danielle, Cheyenne, Raven]; //My mother's children listed in age order//
+
+var grand = [Gabriel, Jazmine]; //My mother's grandchildren listen in age order//
